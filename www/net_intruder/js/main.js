36379b73bf2cbdbd10a42fb844f1ce82e140afeb
Jonathan Giacomelli <jonathan_m_g@live.com>
2019-05-14 15:30:21 -0700

    Update main.js

--- www/net_intruder/js/main.js
+++ www/net_intruder/js/main.js
@@ -14,7 +14,6 @@ function main(){
 	    physics: {
 	        default: 'arcade',
 	        arcade: {
-	            gravity: { y: 300 },
 	            debug: false
 	        }
 	    },
@@ -26,13 +25,18 @@ function main(){
 	};
 
 	var player;
-	var stars;
-	var bombs;
-	var platforms;
 	var cursors;
 	var score = 0;
 	var gameOver = false;
 	var scoreText;
+	
+	var square;
+	var squares;
+	var squarePx = 60;
+	var squareWidth = 9;
+	var squareHeight = 13;
+	
+	var verticalOpenSpace = 80;
 
 	var game = new Phaser.Game(config);
 
@@ -44,6 +48,7 @@ function main(){
 	    // this.load.image('bomb', 'assets/bomb.png');
 	    // this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
 	    this.load.image('player', 'img/quick_player.png');
+	    this.load.image('square', 'img/square_60px.png');
 	}
 
 	function create ()
@@ -65,6 +70,26 @@ function main(){
 
 	    // // The player and its settings
 	    player = this.physics.add.sprite(50, 50, 'player');
+	    square = this.physics.add.sprite(60, 60, 'square');
+	
+		var displayWidth = this.cameras.main.displayWidth;
+		var displayHeight = this.cameras.main.displayHeight;
+		
+		// finds the number of px on each side of play area
+		var horizontalOffset = (displayWidth - (squarePx * squareWidth))/2;
+		
+		// finds the number of px on each side of play area, sans alloted openspace at top
+		var verticalOffset = ((displayHeight - verticalOpenSpace) - (squarePx * squareHeight))/2;
+		var verticalOffsetTop = verticalOpenSpace + verticalOffset;
+		
+		
+		this.squares = this.physics.add.group();
+		for (var i = 0; i < 9; i++) {
+			var x = i * squarePx + horizontalOffset;
+			var y = i * squarePx + verticalOffsetTop;
+
+			var square = this.squares.create(x, y, 'square');
+		}
 
 	    // //  Player physics properties. Give the little guy a slight bounce.
 	    // player.setBounce(0.2);
