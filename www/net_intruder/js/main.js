907e97c4a1dd16577c4e9fe08cf3ca73e60956cd
Jonathan Giacomelli <jonathan_m_g@live.com>
2019-05-14 16:41:50 -0700

    Update main.js

--- www/net_intruder/js/main.js
+++ www/net_intruder/js/main.js
@@ -40,24 +40,24 @@ function main(){
 	var verticalOpenSpace = 80;
     
     var colorPlay = {
-        Red:		"#FF0000",	
-        Maroon:		"#800000",	
-        Yellow:		"#FFFF00",	
-        Olive:		"#808000",	
-        Lime:		"#00FF00",	
-        Green:		"#008000",	
-        Aqua:		"#00FFFF",	
-        Teal:		"#008080",	
-        Blue:		"#0000FF",	
-        Navy:		"#000080",	
-        Fuchsia:	"#FF00FF",	
-        Purple:		"#800080"
+        Red:		"0xFF0000",	
+        Maroon:		"0x800000",	
+        Yellow:		"0xFFFF00",	
+        Olive:		"0x808000",	
+        Lime:		"0x00FF00",	
+        Green:		"0x008000",	
+        Aqua:		"0x00FFFF",	
+        Teal:		"0x008080",	
+        Blue:		"0x0000FF",	
+        Navy:		"0x000080",	
+        Fuchsia:	"0xFF00FF",	
+        Purple:		"0x800080"
     };
     var colorReserved = {
-        White:		"#FFFFFF",
-        Silver:		"#C0C0C0",	
-        Gray:		"#808080",	
-        Black:		"#000000"	
+        White:		"0xFFFFFF",
+        Silver:		"0xC0C0C0",	
+        Gray:		"0x808080",	
+        Black:		"0x000000"	
     };
 
 	var game = new Phaser.Game(config);
