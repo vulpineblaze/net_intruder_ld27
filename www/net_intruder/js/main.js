a187b4da636c4a67664606a6b16f846600dc0469
Jonathan Giacomelli <jonathan_m_g@live.com>
2019-05-15 09:43:55 -0700

    Update main.js

--- www/net_intruder/js/main.js
+++ www/net_intruder/js/main.js
@@ -139,7 +139,7 @@ function main(){
 	function create ()
 	{
         // If this is not a desktop (so it's a mobile device) 
-		console.log("desktop:",game.device.desktop);
+		console.log("desktop:",this.sys.game.device.os.desktop);
         if (game.device.desktop == false) {
             // Set the scaling mode to SHOW_ALL to show all the game
             game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
